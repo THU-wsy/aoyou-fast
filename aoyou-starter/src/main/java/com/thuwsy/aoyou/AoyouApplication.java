f3400d66ee2162a92ce248bb66a4908e3199fb32
THU-wsy <425121632@qq.com>
2024-02-20 13:07:09 +0800

    初始化项目结构

---
+++ aoyou-starter/src/main/java/com/thuwsy/aoyou/AoyouApplication.java
@@ -0,0 +1,21 @@
+package com.thuwsy.aoyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: AoyouApplication
+ * Package: com.thuwsy.aoyou
+ * Description:
+ *
+ * @Author THU_wsy
+ * @Create 2024/2/19 23:42
+ * @Version 1.0
+ */
+// 包名为com.thuwsy.aoyou
+@SpringBootApplication
+public class AoyouApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AoyouApplication.class, args);
+    }
+}
